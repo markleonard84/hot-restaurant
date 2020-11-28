664f6582f133cb148c2d45296e37ffc079282a2e
Mark Leonard <marketh@me.com>
2020-11-28 12:01:08 +0000

    adding routing to JS

--- Server.js
+++ Server.js
@@ -5,19 +5,40 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
+app.use(express.urlencoded({
+    extended: true
+}));
 
+app.use(express.json());
 
 // Data
 // ===========================================================
 
 
 app.get("/:customers", (req, res) => {
-  var chosen = req.params.customers;
-  // What does this log?
-  console.log(chosen);
-  res.end();
+    var chosen = req.params.customers;
+    // What does this log?
+    console.log(chosen);
+    res.end();
 });
 
+
+
+
+
+//routes.
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"))
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"))
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"))
+});
 
 
 
@@ -25,7 +46,5 @@ app.get("/:customers", (req, res) => {
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
-
-
