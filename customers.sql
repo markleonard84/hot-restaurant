fdb3acdaf8440c521f998198c9f127be36306873
Shivey Singh <shivey_khangura@hotmail.com>
2020-12-05 16:45:34 +0000

    created and added database for customer

---
+++ customers.sql
@@ -0,0 +1,20 @@
+/*CREATE TABLE Customers (
+    CustomerID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    EmailAddress varchar(255),
+    ContactNumber varchar(255)
+);*/
+INSERT INTO Customers (LastName, FirstName, EmailAddress, ContactNumber)
+VALUES ('Capone', 'Al', 'alcapone@gmail.com', '07866666666'),
+('Presley', 'Elvis', 'bigel@gmail.com', '07855555555'),
+('Shakur', 'Tupac', '2pac@gmail.com', '07844444444'),
+('Martin', 'Dean', 'dean@gmail.com', '07899999999'),
+('Luciano', 'Lucky', 'lucky7@gmail.com', '07898787565'),
+('Escobar', 'Pablo', 'pb@gmail.com', '07801010101'),
+('Jagger', 'Mick', 'rollingpebbles@gmail.com', '07875894873'),
+('Hendrix', 'Jimmy', 'jimmyhens@gmail.com', '0785873940485'),
+('Sinatra', 'Frank', 'frankie@gmail.com', '078840987'),
+('Marley', 'Bob', 'bigbob@gmail.com', '07898799909'),
+('Einstein', 'Albert', 'clevervato@gmail.com', '07869876789'),
+('Diego', 'Maradonna', 'ilovecocacola@gmail.com', '07869578659');
