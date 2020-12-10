cd294e51ddbe166fe4cc8f2dcee7bf2ff22cc422
Shivey Singh <shivey_khangura@hotmail.com>
2020-12-10 20:36:18 +0000

    Update table.js

--- table.js
+++ table.js
@@ -1,11 +1,6 @@
 let sql = require("mysql");
 let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "Customers",
-  
+ 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
